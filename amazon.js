60f0602721d53bf3a9368692026d520f687432e7
Vvisheshta <Visheshta08@gmail.com>
2026-05-11 15:03:48 +0530

    After making the cart interactive

--- amazon.js
+++ amazon.js
@@ -85,10 +85,13 @@ document.querySelectorAll('.js-add-to-cart').forEach((button) => {
             quantity:1}
           );
         }
-
+        //Adding cartQuantity to the Cart icon
+        let cartQuantity=0;
+        cart.forEach((item)=>{
+          cartQuantity+=item.quantity
+        });
+        document.querySelector('.js-cart-quantity').innerHTML=cartQuantity;
         
-        console.log(cart);
-        console.log(cart);
     });
     
     
