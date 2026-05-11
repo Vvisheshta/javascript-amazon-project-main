ac99e797f554d06342dbeaa7b3830841f63c3b6f
Vvisheshta <Visheshta08@gmail.com>
2026-05-11 21:29:48 +0530

    Save Cart in local storage

--- data/cart.js
+++ data/cart.js
@@ -1,13 +1,19 @@
-export let cart=[
-    {
-        productId: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
-        quantity:2
-    },
-    {
-        productId: "15b6fc6f-327a-4ec4-896f-486349e85a3d",
-        quantity:1
-    }
-];
+export let cart= JSON.parse(localStorage.getItem('cart'));
+//localStorage can only save string, so to convert that to array we use JSON.parse()
+
+if(!cart){
+    cart=[
+        {
+            productId: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
+            quantity:2
+        },
+        {
+            productId: "15b6fc6f-327a-4ec4-896f-486349e85a3d",
+            quantity:1
+        }
+    ];
+}
+
 
 //Add to Cart 
 export function addToCart(productId){
@@ -28,6 +34,7 @@ export function addToCart(productId){
       quantity:1}
     );
   }
+  saveToStorage();
 }
 
 //delete from cart
@@ -38,6 +45,12 @@ export function removeFromCart(productId){
         if(cartItem.productId!==productId){
             newCart.push(cartItem);
         }
+        
     });
     cart=newCart;
+    saveToStorage();
+}
+//saving the cart in localstorage to be unaffected by refresh
+function saveToStorage(){
+    localStorage.setItem('cart',JSON.stringify(cart));
 }
--- scripts/checkout.js
+++ scripts/checkout.js
@@ -20,7 +20,7 @@ cart.forEach((cartItem) => {
 
     cartSummaryHTML= cartSummaryHTML+ `
     
-            <div class="cart-item-container">
+            <div class="cart-item-container js-cart-item-container-${matchingProduct.id}">
             <div class="delivery-date">
               Delivery date: Tuesday, June 21
             </div>
@@ -106,9 +106,10 @@ document.querySelectorAll('.js-delete-link').
     forEach((link) => {
         link.addEventListener(('click'),() => {
             const productId=link.dataset.productId;
-            console.log(productId);
             removeFromCart(productId);
-            console.log(cart);
+            const container=document.querySelector(`.js-cart-item-container-${productId}`);
+            console.log(container);
+            container.remove();
         });
     });
 
